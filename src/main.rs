43d37931f2e667f634110de9cd862bfa08b6e763
Hizbullah Haidar Anis Al Wakil <hizbullah.haidar@dwidasa.com>
2025-12-02 19:42:59 +0700

    lean match

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[allow(dead_code)]
+enum Level {
+    Admin,
+    User,
+    Guest
+}
+
+
+#[test]
+fn test_enum() {
+    let level = Level::Admin;
+    match level {
+        Level::Admin => println!("Admin"),
+        Level::User => println!("User"),
+        Level::Guest => println!("Guest"),
+    }
+}
