c7791586a3e379d529377dbb9abc5c1ce687940a
Hizbullah Haidar Anis Al Wakil <hizbullah.haidar@dwidasa.com>
2025-12-02 19:53:32 +0700

    learn stuct & void

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
 
 
 #[allow(dead_code)]
+#[derive(Debug)]
 enum Level {
     Admin,
     User,
@@ -19,4 +20,39 @@ fn test_enum() {
         Level::User => println!("User"),
         Level::Guest => println!("Guest"),
     }
+}
+
+
+
+struct  Person{
+    name : String,
+    age : u8,
+    level : Level
+}
+
+impl Person {
+    fn new(name: &str, age: u8, level: Level) -> Person {
+        Person {
+            name: name.to_string(),
+            age,
+            level,
+        }
+    }
+
+    fn handle_year(&self ){
+        const MAX_AGE : u8 = 100;
+        if self.age > MAX_AGE {
+            print!("Max")
+        }else{
+            print!("Not Max")
+        }
+
+    }
+}
+
+#[test]
+fn test_void(){
+   let jhon =  Person::new("John", 30, Level::Admin);
+   println!("{} - {:?}", jhon.name, jhon.level);
+   jhon.handle_year();
 }
